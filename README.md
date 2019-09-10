650c1b39c90987f71f6b02ba2a8909ba291bfdef
Leon Li <lileon@amazon.com>
2019-09-10 10:55:46 +0800

    init commits

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # codename-gen
-A simple codename generator tool, free to use for fun.
+### A simple codename generator tool, free to use, its just for fun，you can play it all day long, enjoy!
+
+![Image of the screen](fig/example.png)
+
+The codename generator is based on the imagenet 1000 classes as the source, to install and enjoy the fun, just simply clone the library: 
+
+`git@github.com:leonli/codename-gen.git`
+
+then 
+
+`pip install -r requirements.txt` 
+
+and 
+
+`python3 main.py --count 20` 
+
+you should be all set.
---
+++ main.py
@@ -0,0 +1,23 @@
+import click
+import random
+import pandas as pd
+from pyfiglet import Figlet
+from termcolor import colored, cprint
+import imagenet
+
+@click.command()
+@click.option("--count", default=10, help="Yield number of codenames.")
+def codename_gen(count):
+    """Enjoy the codenames 🍺"""
+    imagenet_cls = imagenet.imagenet1000_labels()
+    f = Figlet(font='slant')
+    print(f.renderText('Codename Gen'))
+    c_tag = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+    for _ in range(count):
+        print(colored(random.choice(imagenet_cls), random.choice(c_tag)))
+    cprint('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', 'green')
+    cprint('Done! Enjoy the codenames 🍺', 'grey', 'on_green')
+
+if __name__ == '__main__':
+    codename_gen()
+
